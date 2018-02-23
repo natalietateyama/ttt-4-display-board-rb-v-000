2f7ef6aaadc97bd39bd110ed73b721f3633c18c2
Natalie Tateyama <natalie.tateyama@gmail.com>
2018-02-23 18:42:55 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,9 +1,7 @@
-def display_board
+def display_board(board)
   puts "   " " | " "   " " | " "   "
   puts "-----------"
   puts "   " " | " "   " " | " "   "
   puts "-----------"
   puts "   " " | " "   " " | " "   "
 end
-# Define display_board that accepts a board and prints
-# out the current state.
