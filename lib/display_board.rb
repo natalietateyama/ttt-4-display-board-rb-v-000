bf1c7094bf6eeb000c0b548324ee46b17fdae253
Natalie Tateyama <natalie.tateyama@gmail.com>
2018-02-23 19:38:22 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,12 +1,4 @@
 def display_board(board)
-  puts "   " " | " "   " " | " "   "
-  puts "-----------"
-  puts "   " " | " "   " " | " "   "
-  puts "-----------"
-  puts "   " " | " "   " " | " "   "
-end
-
-def rows(board)
   puts "#{board[0]}" " | " "#{board[1]}" " | " "#{board[2]}"
   puts "-----------"
   puts "#{board[3]}" " | " "#{board[4]}" " | " "#{board[5]}"
